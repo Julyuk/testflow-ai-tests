382c92ca4bec12b2a08b085f2b221d9a5a29e06d
Yuliia Ukrainets <112850935+Julyuk@users.noreply.github.com>
2026-07-17 16:37:37 +0300

    TestFlow AI live integration test - 2026-07-17T13:36:58Z

---
+++ pages/__init__.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+from pages.login_page import LoginPage
+from pages.inventory_page import InventoryPage
+from pages.cart_page import CartPage
+from pages.checkout_step_one_page import CheckoutStepOnePage
+from pages.checkout_step_two_page import CheckoutStepTwoPage
+from pages.checkout_complete_page import CheckoutCompletePage
+
+__all__ = [
+    "LoginPage",
+    "InventoryPage",
+    "CartPage",
+    "CheckoutStepOnePage",
+    "CheckoutStepTwoPage",
+    "CheckoutCompletePage",
+]
